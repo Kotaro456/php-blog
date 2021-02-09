16dfe23ce816129ef8b2c2d3c7ab81fa78152632
Kotaro456 <kotaro03281616@gmail.com>
2021-02-09 13:16:03 +0900

    first commit and created index.php view

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>投稿一覧</title>
+</head>
+<body>
+    <header>
+        <div class="header-title">
+            <h1>PHP Blog</h1>
+            <h2>PHPで作ったBlog</h2>
+        </div>
+
+        <div class="header-nav">
+            <p><a href="#">新規登録</a></p>
+            <p><a href="#">ログイン</a></p>
+            <p><a href="#">ログアウト</a></p>
+        </div>
+    
+    </header>
+    <div class="main">
+
+        <div class="bloger-profile">
+            <h1>画像</h1>
+            <h2>ブロガーの名前</h2>
+            <p>僕はこういうやつです</p>
+        </div>
+
+        <!-- 記事一覧 -->
+        <div class="contents">
+            <div class="content">
+                <h2>Title</h2>
+                <p>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                </p>
+            </div>
+
+            <div class="content">
+                <h2>Title</h2>
+                <p>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                </p>
+            </div>
+
+            <div class="content">
+                <h2>Title</h2>
+                <p>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                </p>
+            </div>
+
+            <div class="content">
+                <h2>Title</h2>
+                <p>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                </p>
+            </div>
+
+            <div class="content">
+                <h2>Title</h2>
+                <p>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                    blogcontentblogcontentblogcontentblogcontentblogcontentblogcontent<br>
+                </p>
+            </div>
+
+            <!-- ページング -->
+            <div class="pages">
+                <ul>
+                    <li><a href="#">前のページへ</a></li>
+                    <li><a href="#">次のページへ</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>〇〇のブログ</p>
+    </footer>
+</body>
+</html>
