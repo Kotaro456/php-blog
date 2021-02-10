66d2bf28d8a94b197c82faa6f852a499c3606aa7
Kotaro456 <kotaro03281616@gmail.com>
2021-02-10 18:09:59 +0900

    created auth files and views

---
+++ auth/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+
+<body>
+    <header>
+        <div class="header-title">
+            <h1>新規登録</h1>
+        </div>
+
+        <div class="header-nav">
+            <p><a href="#">ログイン</a></p>
+        </div>
+    </header>
+
+    <div class="main">
+
+        <!-- ログインフォーム -->
+        <form action="#" method="post">
+
+            <input type="email" name="email" placeholder="メアド" />
+
+            <input type="password" name="password" placeholder="パスワード" />
+
+            <input type="submit" value="ログイン" />
+
+        </form>
+    </div>
+
+</body>
+
+</html>
---
+++ auth/register.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新規登録</title>
+</head>
+<body>
+    <header>
+        <div class="header-title">
+            <h1>新規登録</h1>
+        </div>
+
+        <div class="header-nav">
+            <p><a href="#">ログイン</a></p>
+        </div>
+    </header>
+
+    <div class="main">
+
+        <!-- 登録フォーム -->
+        <form action="#" method="post">
+            <input type="text" name="name" placeholder="名前" />
+
+            <input type="email" name="email" placeholder="メアド" />
+
+            <input type="password" name="password" placeholder="パスワード" />
+
+            <input type="password" name="check_password" placeholder="パスワード確認" />
+
+            <input type="submit" value="登録する" />
+
+        </form>
+    </div>
+
+</body>
+</html>
