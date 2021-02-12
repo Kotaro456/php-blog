44236b8228aa1608e850dfd9dac8b4792c45064a
Kotaro456 <kotaro03281616@gmail.com>
2021-02-12 13:16:07 +0900

    created basic create func

--- post/create.php
+++ post/create.php
@@ -1,3 +1,36 @@
+<?php
+    // DB接続
+    require('../dbconnect.php');
+
+    // DBに保存するまでの流れ↓
+    // エラーメッセージ用の変数を用意しておく
+    $errorTitle = '';
+    $errorContent = '';
+
+
+    // $_POSTが空っぽじゃない時にDBに保存するまでの操作を開始する
+    if(!empty($_POST)) {
+
+        // フォームの値を変数に入れておく htmlspecialcharsでHTMLエンティティ化する
+        $title = htmlspecialchars($_POST['title'], ENT_QUOTES);
+        $content = htmlspecialchars($_POST['content'], ENT_QUOTES);
+
+    
+
+            
+            // SQL文を直書きはまずいので、サニタイズ
+            $sth = $db->prepare('INSERT INTO posts (title, content, created) values (?, ?, now())');
+            $sth->execute(array($title, $content));
+
+
+            header('Location:/php-blog/index.php/');
+            
+
+    }else{
+        echo '入力してください';
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -21,7 +54,9 @@
     </header>
     <div class="main">
         <h1>記事作成</h1>
-        <form action="#" method="post">
+
+        <!-- action=""はフォームの送信先をこのページに指定するという意味 -->
+        <form action="create.php" method="post">
             <input type="text" name="title" placeholder="記事のタイトル"/>
 
             <textarea type="text" name="content" placeholder="記事の内容"></textarea>
