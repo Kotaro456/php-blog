af1e63dbb4049dce583ef3bae9c46520fbcdc429
Kotaro456 <kotaro03281616@gmail.com>
2021-02-16 13:14:43 +0900

    deleted update.php and move update func to edit.php

--- post/edit.php
+++ post/edit.php
@@ -12,6 +12,22 @@ $statement->execute(array($id));
 $post = $statement->fetch();
 
 
+// 編集後の操作
+// 編集後のDB操作
+if (!empty($_POST)) {
+
+    $update_id = $_POST['id'];
+    $new_title = htmlspecialchars($_POST['new_title']);
+    $new_content = htmlspecialchars($_POST['new_content']);
+    
+    // 指定の値をアップデートするためのSQL文
+    $new_post = $db->prepare('UPDATE posts SET title=?, content=?, modified=now() WHERE id=?');
+    $new_post->execute(array($new_title, $new_content, $update_id));
+    
+    header('Location: index.php');
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +44,7 @@ $post = $statement->fetch();
     <?php include('../layouts/header.html') ?>
     <div class="main">
         <h1>記事作成</h1>
-        <form action="update.php" method="post">
+        <form action="edit.php" method="post">
             <input type="text" name="new_title" placeholder="記事のタイトル" value="<?php echo $post['title']; ?>" />
 
             <textarea type="text" name="new_content" placeholder="記事の内容"><?php echo $post['content']; ?></textarea>
